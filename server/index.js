f8bb20168930e92b7d806b9782289c831260c50e
AC <a.chaurasia@sap.com>
2023-05-02 20:00:54 +0200

    cors body parser helmet morgan

--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
 import express from "express";
+import bodyParser from "body-parser";
+import helmet from "helmet";
+import cors from "cors";
+import morgan from "morgan";
 
 const app = express();
+
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan("combined"));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
