250af4e51ddf3a2c7299b852730f88c179d6bb90
AC <a.chaurasia@sap.com>
2023-05-03 23:41:09 +0200

    add mongo db 🚀

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
+import mongoose from "mongoose";
 import app from "./app.js";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-app.listen(process.env.PORT, () => {
-  console.log("App listening on port 3000!");
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log("App listening on port 3000!");
+  });
 });
 
 export default app;
