01e1f6a80feb4da2168ae6f4d55ee3c8c57dd7cc
AC <a.chaurasia@sap.com>
2023-05-03 22:16:53 +0200

    add .env ♻️

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import bodyParser from "body-parser";
 import helmet from "helmet";
 import cors from "cors";
 import morgan from "morgan";
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
 
 const app = express();
 
@@ -15,7 +17,7 @@ app.get("/ping", (req, res) => {
   res.send("pong");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("App listening on port 3000!");
 });
 
